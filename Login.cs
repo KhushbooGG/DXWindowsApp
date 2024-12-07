3aa6b90e7bd20822954661e6b58d8bd51a07f171
KhushbooGG <khushbo.goel285@gmail.com>
2024-12-08 00:08:49 +0530

    database connectivity done

--- Login.cs
+++ Login.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DXWindowsApp
 {
@@ -25,17 +28,25 @@ namespace DXWindowsApp
             string password = textBox2.Text;
 
             // For Student Login
-            if (username == "admin" && password == "admin123")
+            SqlConnection sqlConnection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["MyDBConnection"].ConnectionString);
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Users WHERE Username = @username AND Password = @password AND IsActive=1", sqlConnection);
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", password);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
             {
                 // Redirect to another form
                 this.Hide();
                 StudentDeshBoard deshboard = new StudentDeshBoard();
                 deshboard.Show();
             }
-            else 
+            else
             {
-             string message = "Invalid Username or Password";
-             label3.Text = message;  
+                string message = "Invalid Username or Password";
+                label3.Text = message;
             }
 
         }
